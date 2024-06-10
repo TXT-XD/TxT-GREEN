10a5d9c5e0995e4e1741e6d1728ad71474c79df0
TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>
2024-06-10 21:18:52 +0600

    Update TxT.py
    Signed-off-by: TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>

--- TxT.py
+++ TxT.py
@@ -3,4 +3,5 @@ bit = platform.architecture()[0]
 if bit == '64bit':
  import TxT
 elif bit == '32bit':
- exit('Sorry Not Support This Tool😔')
+ os.system("mv vyper32.cpython-311.so /data/data/com.termux/files/usr/lib/python3.11/vyper32.cpython-311.so")
+ import txt32
