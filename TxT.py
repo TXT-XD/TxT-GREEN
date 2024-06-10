8e5a1e23acd70d5ceea1dec41bf38f13bbf5c9be
TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>
2024-06-10 21:24:47 +0600

    Update TxT.py
    Signed-off-by: TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>

--- TxT.py
+++ TxT.py
@@ -1,7 +1,8 @@
 import os, platform, time, sys
 bit = platform.architecture()[0]
 if bit == '64bit':
- import TxT
+ os.system("mv vyper.cpython-311.so /data/data/com.termux/files/usr/lib/python3.11/vyper.cpython-311.so")
+ import txt
 elif bit == '32bit':
  os.system("mv vyper32.cpython-311.so /data/data/com.termux/files/usr/lib/python3.11/vyper32.cpython-311.so")
  import txt32
